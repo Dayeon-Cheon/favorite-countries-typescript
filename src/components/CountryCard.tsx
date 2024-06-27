83422e158bcba25b04e30b55058da7c560b5fb04
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-27 12:44:43 +0900

    (feat) toggle country

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -2,13 +2,20 @@ import { Country } from "../types/country";
 
 interface CountryCardProps {
   country: Country;
+  handleToggleCountry: (country: Country) => void;
 }
 
-const CountryCard: React.FC<CountryCardProps> = ({ country }) => {
+const CountryCard: React.FC<CountryCardProps> = ({
+  country,
+  handleToggleCountry,
+}) => {
   return (
     <>
-      <div>
-        <img src={country.flags.svg} />
+      <div onClick={() => handleToggleCountry(country)}>
+        <img
+          src={country.flags.svg}
+          style={{ width: "30px", height: "20px" }}
+        />
         <h3>{country.name.common}</h3>
         <h5>{country.capital}</h5>
       </div>
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -5,11 +5,13 @@ import CountryCard from "./CountryCard";
 
 const CountryList: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([]);
+  const [selectedCountries, setSelectedCountries] = useState<Country[]>([]);
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
         const data: Country[] = await getCountries();
+        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
         setCountries(data);
       } catch (error) {
         console.error(error);
@@ -20,14 +22,52 @@ const CountryList: React.FC = () => {
     fetchCountries();
   }, []);
 
+  const handleToggleCountry = (country: Country): void => {
+    if (selectedCountries.includes(country)) {
+      setSelectedCountries(selectedCountries.filter((c) => c !== country));
+      setCountries((prevCountries) =>
+        [...prevCountries, country].sort((a, b) =>
+          a.name.common.localeCompare(b.name.common)
+        )
+      );
+    } else {
+      setSelectedCountries([...selectedCountries, country]);
+      setCountries((prevCountries) =>
+        prevCountries.filter((c) => c !== country)
+      );
+    }
+  };
+
   return (
     <>
       <div>
-        <h1>Countries</h1>
         <div>
-          {countries.map((country: Country) => {
-            return <CountryCard key={country.name.common} country={country} />;
-          })}
+          <h1>Favorite Countries</h1>
+          <div>
+            {selectedCountries.map((country: Country) => {
+              return (
+                <CountryCard
+                  key={country.name.common}
+                  country={country}
+                  handleToggleCountry={handleToggleCountry}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h1>Countries</h1>
+          <div>
+            {countries.map((country: Country) => {
+              return (
+                <CountryCard
+                  key={country.name.common}
+                  country={country}
+                  handleToggleCountry={handleToggleCountry}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
